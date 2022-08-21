6521073d4fc65aa97ab21ad43e44860092bcadcd
Saar Yogev <saaryogev@gmail.com>
2022-08-21 22:34:08 +0300

    Added Dockerfile and ignore files, updated main to create a poll object.

--- vote_server/src/main.rs
+++ vote_server/src/main.rs
@@ -1,17 +1,35 @@
 #[macro_use]
 extern crate rocket;
 
-use chrono::{DateTime, Utc};
-use rocket::form::Form;
+use std::string::String;
+use std::future::Future;
+use chrono::{DateTime, TimeZone, Utc};
+use rocket::form::{Form, FromFormField};
 use rocket::serde::{Deserialize};
+use rocket::serde::json::Json;
+use rocket::State;
+use uuid::Uuid;
 
-#[post("/poll", data = "<poll>")]
-fn create_poll(poll: Form<PollCreationDetails>) -> String {
-    format!("Creating a poll named {}, closing at", poll.name/*, poll.close_date*/)
+#[post("/poll", format = "json", data = "<poll_details>")]
+fn create_poll(poll_details: Json<PollCreationDetails>) -> String {
+    let poll = Poll {
+        name: poll_details.name.clone(),
+        uuid: Uuid::new_v4(),
+        targets: poll_details.targets.iter().map(|target_string| Target {
+            name: target_string.to_string(),
+            uuid: Uuid::new_v4(),
+            votes: 0,
+        }).collect(),
+        start_date: Utc::now().to_string(),
+        close_date: poll_details.close_date.to_string(),
+    };
+    let output = format!("Creating a poll named {}, closing at {}", &poll.name, &poll.close_date);
+    return output;
 }
 
 #[launch]
 fn rocket() -> _ {
+    println!("rocket launched!");
     rocket::build().mount("/", routes![create_poll])
 }
 
@@ -22,15 +40,16 @@ pub struct Vote {
 
 pub struct Target {
     pub name: String,
-    pub guid: String,
+    pub uuid: Uuid,
     pub votes: i64,
 }
 
 pub struct Poll {
     pub name: String,
+    pub uuid: Uuid,
     pub targets: Vec<Target>,
-    pub start_date: DateTime<Utc>,
-    pub close_date: DateTime<Utc>,
+    pub start_date: String,
+    pub close_date: String,
 }
 
 #[derive(Deserialize, FromForm)]
@@ -38,15 +57,15 @@ pub struct Poll {
 struct PollCreationDetails {
     name: String,
     targets: Vec<String>,
-    // close_date: DateTime<Utc>,
+    close_date: String,
 }
 
 impl PollCreationDetails {
-    fn new(name: String, targets: Vec<String>/*, close_date: DateTime<Utc>*/) -> Self {
+    fn new(name: String, targets: Vec<String>, close_date: String) -> Self {
         Self {
             name,
             targets,
-            // close_date
+            close_date,
         }
     }
 }
