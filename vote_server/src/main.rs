479d8aba9c28d9daf64c9ccd3f410c31f6973efa
Saar Yogev <saaryogev@gmail.com>
2024-01-20 21:58:58 +0200

    Fixed the dockerfile added some error handling to choice API.

--- vote_server/src/main.rs
+++ vote_server/src/main.rs
@@ -41,13 +41,16 @@ async fn create_choice(choice_details: Json<ChoiceCreationDetails>) -> String {
     let choice = Choice {
         name: choice_details.name.clone(),
         uuid: Uuid::new_v4(),
-        poll_uuid: choice_details.poll_uuid.parse().unwrap()
+        poll_uuid: choice_details.poll_uuid.parse().unwrap(),
     };
 
-    insert_into(choices).values(&choice).execute(&get_connection());
+    let choice_query_result = insert_into(choices).values(&choice).execute(&get_connection());
 
-    let output = format!("Creating a choice named {}, for poll {}", &choice.name, &choice.poll_uuid);
-    return output;
+    match choice_query_result {
+        Ok(_) => { return format!("Creating a choice named {}, for poll {}", &choice.name, &choice.poll_uuid); }
+        Err(error) => { panic!("{}", error)}
+    }
+
 }
 
 #[post("/vote", format = "json", data = "<vote_details>")]
@@ -55,13 +58,15 @@ async fn cast_vote(vote_details: Json<VoteCreationDetails>) -> String {
     let vote = Vote {
         uuid: Uuid::new_v4(),
         signature: vote_details.signature.clone(),
-        choice_uuid: vote_details.choice_uuid.parse().unwrap()
+        choice_uuid: vote_details.choice_uuid.parse().unwrap(),
     };
 
-    insert_into(votes).values(&vote).execute(&get_connection());
+    let vote_query_result = insert_into(votes).values(&vote).execute(&get_connection());
 
-    let output = format!("casting a vote with the signature {}, for choice {}", &vote.signature, &vote.choice_uuid);
-    return output;
+    match vote_query_result {
+        Ok(_) => { return format!("Casting a vote with the signature {}, for choice {}", &vote.signature, &vote.choice_uuid); }
+        Err(error) => { panic!("{}", error)}
+    }
 }
 
 fn get_connection() -> PgConnection {
